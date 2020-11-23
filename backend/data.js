ae59ce2f46865585483b37a2b48934bb0e27a35c
Brian Fleming <59600239+brianFleming-coder@users.noreply.github.com>
2020-11-23 16:04:06 +0000

    update mongodb

--- backend/data.js
+++ backend/data.js
@@ -1,27 +1,25 @@
-import bcrypt from "bcryptjs";
+import bcrypt from 'bcryptjs';
 
 const data = {
   users: [
     {
-      name: "Brian",
-      email: "admin@example.com",
-      password: bcrypt.hashAsync (`1234`, 8),
+      name: 'Basir',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('1234', 8),
       isAdmin: true,
     },
     {
-      name: "John",
-      email: "user@example.com",
-      password: bcrypt.hashAsync (`5678`, 8),
+      name: 'John',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('1234', 8),
       isAdmin: false,
     },
-
   ],
   products: [
     {
-      _id: '1',
-      name: 'consectetur adipiscing elit',
+      name: 'Nike Slim Shirt',
       category: 'Shirts',
-      image: 'https://picsum.photos/200/300.jpg',
+      image: '/images/p1.jpg',
       price: 120,
       countInStock: 10,
       brand: 'Nike',
@@ -30,10 +28,9 @@ const data = {
       description: 'high quality product',
     },
     {
-      _id: '2',
       name: 'Adidas Fit Shirt',
       category: 'Shirts',
-      image: 'https://picsum.photos/200/300/?blur.jpg',
+      image: '/images/p2.jpg',
       price: 100,
       countInStock: 20,
       brand: 'Adidas',
@@ -42,10 +39,9 @@ const data = {
       description: 'high quality product',
     },
     {
-      _id: '3',
       name: 'Lacoste Free Shirt',
       category: 'Shirts',
-      image: 'https://picsum.photos/200/300.jpg',
+      image: '/images/p3.jpg',
       price: 220,
       countInStock: 0,
       brand: 'Lacoste',
@@ -54,10 +50,9 @@ const data = {
       description: 'high quality product',
     },
     {
-      _id: '4',
       name: 'Nike Slim Pant',
       category: 'Pants',
-      image: 'https://picsum.photos/200/300.jpg',
+      image: '/images/p4.jpg',
       price: 78,
       countInStock: 15,
       brand: 'Nike',
@@ -66,10 +61,9 @@ const data = {
       description: 'high quality product',
     },
     {
-      _id: '5',
       name: 'Puma Slim Pant',
       category: 'Pants',
-      image: 'https://picsum.photos/200/300.jpg',
+      image: '/images/p5.jpg',
       price: 65,
       countInStock: 5,
       brand: 'Puma',
@@ -78,10 +72,9 @@ const data = {
       description: 'high quality product',
     },
     {
-      _id: '6',
       name: 'Adidas Fit Pant',
       category: 'Pants',
-      image: 'https://picsum.photos/200/300.jpg',
+      image: '/images/p6.jpg',
       price: 139,
       countInStock: 12,
       brand: 'Adidas',
--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,9 +1,17 @@
-import express from "express";
-import data from "../data.js";
-import User from "../models/userModel";
+import express from 'express';
+import expressAsyncHandler from 'express-async-handler';
+import data from '../data.js';
+import User from '../models/userModel.js';
 
 const userRouter = express.Router();
 
-userRouter.get("/seed", async(req, res) => {
-    const createUser = await User.insertMany(data.users);
-});
+userRouter.get(
+  '/seed',
+  expressAsyncHandler(async (req, res) => {
+    // await User.remove({});
+    const createdUsers = await User.insertMany(data.users);
+    res.send({ createdUsers });
+  })
+);
+
+export default userRouter;
